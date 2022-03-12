e6ff4067c9a833a96eba30fc2264f2a3e55321a1
Vitaly <msk.vitaly@gmail.com>
2022-03-12 22:36:00 +0300

    Mobile version

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,14 +11,26 @@ import styles from '../styles/Home.module.scss'
  */
 const Home: NextPage = () => {
 
-  //*------------------------------------------ Visual state
-  const activeCopy = useRef<HTMLButtonElement>(null)
+  //*------------------------------------------ Visual states
+  const copyBtn = useRef<HTMLButtonElement>(null)
+  const saveBtn = useRef<HTMLButtonElement>(null)
+
+  function showActive(ref: HTMLButtonElement | null) {
+    ref?.focus();
+    ref?.classList.toggle("active_button");
+    setTimeout(() => {
+      ref?.blur();
+      ref?.classList.toggle("active_button");
+    }, 2000)
+  }
   function showCopied() {
-    activeCopy.current?.focus()
-    setTimeout(() => activeCopy.current?.blur(), 2000)
+    showActive(copyBtn.current)
+  }
+  function showSaved() {
+    showActive(saveBtn.current)
   }
 
-  //*------------------------------------------ Canvas
+  //*------------------------------------------ Canvas Interaction
   const signCanvas = useRef<CanvasDraw>(null);
   function clear() {
     signCanvas.current?.clear()
@@ -26,12 +38,23 @@ const Home: NextPage = () => {
   function undo() {
     signCanvas.current?.undo()
   }
-  async function save() {
+  async function copy() {
     const src = (signCanvas.current as any).getDataURL()
     const blob = await (await fetch(src)).blob()
     const clipboardItem = new ClipboardItem({ [blob.type]: blob });
     navigator.clipboard.write([clipboardItem]);
     showCopied()
+  }
+  function save() {
+    const src = (signCanvas.current as any).getDataURL()
+    const link = document.createElement('a');
+    link.href = src;
+    link.download = 'sign.png';
+    link.style.display = 'none';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    showSaved()
   }
 
   //*------------------------------------------ Canvas settings
@@ -45,7 +68,7 @@ const Home: NextPage = () => {
   //*------------------------------------------ Keys
   function handleKeyDown({ code, ctrlKey }: { code: string, ctrlKey?: boolean }) {
     if (code === "Delete") clear()
-    else if (ctrlKey && code === "KeyC") save()
+    else if (ctrlKey && code === "KeyC") copy()
     else if (ctrlKey && code === "KeyZ") undo()
   }
 
@@ -82,8 +105,7 @@ const Home: NextPage = () => {
 
     <main className={styles.main}>
       <h1 className={styles.title}>Sign-app 🖋️</h1>
-      <p>It was built to try something</p>
-
+      <p className='mobile-hide'>It was built to try something</p>
 
       <div className={styles.editor}>
         <CanvasDraw
@@ -92,32 +114,53 @@ const Home: NextPage = () => {
           brushColor={settings.brushColor}
           lazyRadius={settings.lazyRadius}
           brushRadius={settings.lazyRadius}
+          hideInterface={true}
         />
 
         <div className={styles.settings}>
-          <div>
-            <button className={styles.setting_button} onClick={undo}>[Ctrl+Z] Undo</button>
-            <button className={styles.setting_button} onClick={clear}>[Delete] Clear</button>
+          <div className={styles.columns_2}>
+            <button className={styles.setting_button} onClick={undo}>
+              <span className='mobile-hide'>[Ctrl+Z]</span>Undo
+            </button>
+            <button className={styles.setting_button} onClick={clear}>
+              <span className='mobile-hide'>[Delete]</span>Clear
+            </button>
           </div>
 
           <div className={styles.flex_column}>
-            <label htmlFor="lazyRadius">Brush size</label>
-            <input type="range" min={1} max={10} id="lazyRadius"
-              value={settings.lazyRadius}
-              onChange={({ target }) => setLazyRadius(+target.value)} />
-
-            <label htmlFor="brushColor">Ink</label>
-            <input type="color" id="brushColor"
-              value={settings.brushColor}
-              onChange={({ target }) => setBrushColor(target.value)} />
+            <div className={styles.columns_2} >
+              <label htmlFor="lazyRadius">Brush size</label>
+              <input type="range" min={1} max={10} id="lazyRadius"
+                value={settings.lazyRadius}
+                onChange={({ target }) => setLazyRadius(+target.value)} />
+            </div>
+            <div className={styles.columns_2}>
+              <label htmlFor="brushColor">Ink</label>
+              <input type="color" id="brushColor"
+                value={settings.brushColor}
+                onChange={({ target }) => setBrushColor(target.value)} />
+            </div>
           </div>
 
-          <button
-            ref={activeCopy}
-            onClick={save}
-            className={styles.setting_button}>
-            [Ctrl+C] Copy
-          </button>
+          <div className={styles.columns_2}>
+            <button
+              ref={copyBtn}
+              onClick={copy}
+              className={styles.setting_button}>
+              {copyBtn.current?.classList.contains("active_button") ?
+                "Copied 👌" :
+                <><span className='mobile-hide'>[Ctrl+C]</span> Copy</>}
+            </button>
+
+            <button
+              ref={saveBtn}
+              onClick={save}
+              className={styles.setting_button}>
+              {saveBtn.current?.classList.contains("active_button") ?
+                "Saved 👌" :
+                <><span className='mobile-hide'>[Ctrl+S]</span> Save</>}
+            </button>
+          </div>
         </div>
       </div>
     </main >
