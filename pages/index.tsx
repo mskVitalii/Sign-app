e4522dcf031c7dea5f22028b2e7bbd3593f995d6
mskKote <msk.vitaly@gmail.com>
2024-09-01 19:09:39 +0200

    refactor: styles & links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,54 @@
-import { useState, useEffect, useRef } from 'react'
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import CanvasDraw, { CanvasDrawProps } from "react-canvas-draw";
-import styles from '../styles/Home.module.scss'
-
+import { useState, useEffect, useRef, useCallback } from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import CanvasDraw, { CanvasDrawProps } from 'react-canvas-draw';
+import styles from '../styles/Home.module.scss';
 
 /**
  * Canvas itself was built by react-canvas-draw;
  * For keyboard actions I use "useUffect" hook for client-side window API;
  */
 const Home: NextPage = () => {
-
   //*------------------------------------------ Visual states
-  const copyBtn = useRef<HTMLButtonElement>(null)
-  const saveBtn = useRef<HTMLButtonElement>(null)
+  const copyBtn = useRef<HTMLButtonElement>(null);
+  const saveBtn = useRef<HTMLButtonElement>(null);
 
   function showActive(ref: HTMLButtonElement | null) {
     ref?.focus();
-    ref?.classList.toggle("active_button");
+    ref?.classList.toggle('active_button');
     setTimeout(() => {
       ref?.blur();
-      ref?.classList.toggle("active_button");
-    }, 2000)
+      ref?.classList.toggle('active_button');
+    }, 2000);
   }
-  function showCopied() {
-    showActive(copyBtn.current)
-  }
-  function showSaved() {
-    showActive(saveBtn.current)
-  }
+
+  const showCopied = useCallback(() => {
+    showActive(copyBtn.current);
+  }, []);
+
+  const showSaved = useCallback(() => {
+    showActive(saveBtn.current);
+  }, []);
 
   //*------------------------------------------ Canvas Interaction
   const signCanvas = useRef<CanvasDraw>(null);
-  function clear() {
-    signCanvas.current?.clear()
-  }
-  function undo() {
-    signCanvas.current?.undo()
-  }
-  async function copy() {
-    const src = (signCanvas.current as any).getDataURL()
-    const blob = await (await fetch(src)).blob()
+  const clear = useCallback(() => {
+    signCanvas.current?.clear();
+  }, []);
+  const undo = useCallback(() => {
+    signCanvas.current?.undo();
+  }, []);
+
+  const copy = useCallback(async () => {
+    const src = (signCanvas.current as any).getDataURL();
+    const blob = await (await fetch(src)).blob();
     const clipboardItem = new ClipboardItem({ [blob.type]: blob });
-    navigator.clipboard.write([clipboardItem]);
-    showCopied()
-  }
-  function save() {
-    const src = (signCanvas.current as any).getDataURL()
+    await navigator.clipboard.write([clipboardItem]);
+    showCopied();
+  }, [showCopied]);
+
+  const save = useCallback(() => {
+    const src = (signCanvas.current as any).getDataURL();
     const link = document.createElement('a');
     link.href = src;
     link.download = 'sign.png';
@@ -54,125 +56,173 @@ const Home: NextPage = () => {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-    showSaved()
-  }
+    showSaved();
+  }, [showSaved]);
 
   //*------------------------------------------ Canvas settings
   const [settings, setSettings] = useState<CanvasDrawProps>({
-    brushColor: "#000000",
-    lazyRadius: 3
-  })
-  function setBrushColor(brushColor: string) { setSettings(x => ({ ...x, brushColor })) }
-  function setLazyRadius(lazyRadius: number) { setSettings(x => ({ ...x, lazyRadius })) }
-
-  //*------------------------------------------ Keys
-  function handleKeyDown({ code, ctrlKey }: { code: string, ctrlKey?: boolean }) {
-    if (code === "Delete") clear()
-    else if (ctrlKey && code === "KeyC") copy()
-    else if (ctrlKey && code === "KeyZ") undo()
+    brushColor: '#00008B',
+    lazyRadius: 1,
+  });
+  function setBrushColor(brushColor: string) {
+    setSettings((x) => ({ ...x, brushColor }));
+  }
+  function setLazyRadius(lazyRadius: number) {
+    setSettings((x) => ({ ...x, lazyRadius }));
   }
 
+  //*------------------------------------------ Keys
+  const handleKeyDown = useCallback(
+    ({ code, ctrlKey }: { code: string; ctrlKey?: boolean }) => {
+      if (code === 'Delete') clear();
+      else if (ctrlKey && code === 'KeyC') copy();
+      else if (ctrlKey && code === 'KeyZ') undo();
+    },
+    [clear, copy, undo]
+  );
+
   useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
 
-  return (<div className={styles.container}>
-    <Head>
-      <title>Sign-app</title>
-      <link rel="icon" href="/icon.svg" />
-      {/* Common */}
-      <meta charSet="UTF-8" />
-      <meta name="description" content="web-app for sign drawing" />
-      <meta name="keywords" content="Sign, draw sign" />
-      <meta name="viewport" content="width=device-width" />
-      <meta name="Author" content="Popov Vitaly" />
-      {/* Social networks */}
-      <meta property="og:locale" content="en_EN" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="Sign-app" />
-      <meta property="og:site_name" content="Sign-app" />
-      <meta property="og:description" content="web-app for sign drawing" />
-      <meta property="og:image" content="https://sign-app-mu.vercel.app/icon.svg" />
-      <meta property="og:url" content="https://sign-app-mu.vercel.app" />
-      {/* Twitter */}
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="Popov Vitaly" />
-      <meta name="twitter:title" content="Sign, draw sign" />
-      <meta name="twitter:description" content="web-app for sign drawing" />
-      <meta name="twitter:image" content="https://sign-app-mu.vercel.app/icon.svg" />
-    </Head>
-
-    <main className={styles.main}>
-      <h1 className={styles.title}>Sign-app 🖋️</h1>
-      <p className='mobile-hide'>It was built to try something</p>
-
-      <div className={styles.editor}>
-        <CanvasDraw
-          ref={signCanvas}
-          className={styles.canvas}
-          brushColor={settings.brushColor}
-          lazyRadius={0}
-          brushRadius={settings.lazyRadius}
-          hideInterface={true}
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Sign-app</title>
+        <link rel='icon' href='/icon.svg' />
+        {/* Common */}
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width' />
+        <meta
+          name='description'
+          content='Create and save your digital signatures easily with our intuitive sign drawing tool'
         />
+        <meta name='keywords' content='Sign, draw sign, docs signing' />
+        <meta name='Author' content='Popov Vitalii' />
+        <meta name='distribution' content='global' />
+        {/* Social networks */}
+        <meta property='og:locale' content='en' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Sign-app' />
+        <meta property='og:site_name' content='Sign-app' />
+        <meta
+          name='og:description'
+          content='Create and save your digital signatures easily with our intuitive sign drawing tool'
+        />
+        <meta
+          property='og:image'
+          content='https://sign-app-mu.vercel.app/icon.svg'
+        />
+        <meta property='og:url' content='https://sign-app-mu.vercel.app' />
+        {/* Twitter */}
+        <meta name='twitter:card' content='summary' />
+        <meta name='twitter:site' content='Popov Vitalii' />
+        <meta name='twitter:title' content='Sign, draw sign' />
+        <meta
+          name='twitter:description'
+          content='Create and save your digital signatures easily with our intuitive sign drawing tool'
+        />
+        <meta
+          name='twitter:image'
+          content='https://sign-app-mu.vercel.app/icon.svg'
+        />
+      </Head>
 
-        <div className={styles.settings}>
-          <div className={styles.columns_2}>
-            <button className={styles.setting_button} onClick={undo}>
-              <span className='mobile-hide'>[Ctrl+Z]</span>Undo
-            </button>
-            <button className={styles.setting_button} onClick={clear}>
-              <span className='mobile-hide'>[Delete]</span>Clear
-            </button>
-          </div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Sign-app 🖋️</h1>
 
-          <div className={styles.flex_column}>
-            <div className={styles.columns_2} >
-              <label htmlFor="lazyRadius">Brush size</label>
-              <input type="range" min={1} max={10} id="lazyRadius"
-                value={settings.lazyRadius}
-                onChange={({ target }) => setLazyRadius(+target.value)} />
-            </div>
+        <div className={styles.editor}>
+          <CanvasDraw
+            ref={signCanvas}
+            className={styles.canvas}
+            brushColor={settings.brushColor}
+            lazyRadius={0}
+            brushRadius={settings.lazyRadius}
+            hideInterface={true}
+          />
+
+          <div className={styles.settings}>
             <div className={styles.columns_2}>
-              <label htmlFor="brushColor">Ink</label>
-              <input type="color" id="brushColor"
-                value={settings.brushColor}
-                onChange={({ target }) => setBrushColor(target.value)} />
+              <button className={styles.setting_button} onClick={undo}>
+                <span className='mobile-hide shortcut'>[Ctrl+Z]</span>Undo
+              </button>
+              <button className={styles.setting_button} onClick={clear}>
+                <span className='mobile-hide shortcut'>[Delete]</span>Clear
+              </button>
             </div>
-          </div>
 
-          <div className={styles.columns_2}>
-            <button
-              ref={copyBtn}
-              onClick={copy}
-              className={styles.setting_button}>
-              {copyBtn.current?.classList.contains("active_button") ?
-                "Copied 👌" :
-                <><span className='mobile-hide'>[Ctrl+C]</span> Copy</>}
-            </button>
+            <div className={styles.flex_column}>
+              <div className={styles.columns_2}>
+                <label htmlFor='lazyRadius'>Brush size</label>
+                <input
+                  type='range'
+                  min={0.5}
+                  max={3}
+                  step={0.5}
+                  id='lazyRadius'
+                  value={settings.lazyRadius}
+                  onChange={({ target }) => setLazyRadius(+target.value)}
+                />
+              </div>
+              <div className={styles.columns_2}>
+                <label htmlFor='brushColor'>Ink</label>
+                <input
+                  type='color'
+                  id='brushColor'
+                  value={settings.brushColor}
+                  onChange={({ target }) => setBrushColor(target.value)}
+                />
+              </div>
+            </div>
 
-            <button
-              ref={saveBtn}
-              onClick={save}
-              className={styles.setting_button}>
-              {saveBtn.current?.classList.contains("active_button") ?
-                "Saved 👌" :
-                <><span className='mobile-hide'>[Ctrl+S]</span> Save</>}
-            </button>
+            <div className={styles.columns_2}>
+              <button
+                ref={copyBtn}
+                onClick={copy}
+                className={styles.setting_button}
+              >
+                {copyBtn.current?.classList.contains('active_button') ? (
+                  'Copied 👌'
+                ) : (
+                  <>
+                    <span className='mobile-hide shortcut'>[Ctrl+C]</span> Copy
+                  </>
+                )}
+              </button>
+
+              <button
+                ref={saveBtn}
+                onClick={save}
+                className={styles.setting_button}
+              >
+                {saveBtn.current?.classList.contains('active_button') ? (
+                  'Saved 👌'
+                ) : (
+                  <>
+                    <span className='mobile-hide shortcut'>[Ctrl+S] </span> Save
+                  </>
+                )}
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </main >
+      </main>
 
+      <footer className={styles.footer}>
+        Author:
+        <a
+          href='https://linktr.ee/mskVitalii'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Popov Vitalii
+        </a>
+      </footer>
+    </div>
+  );
+};
 
-    <footer className={styles.footer}>
-      Author:
-      <a href="https://github.com/mskKote" target="_blank" rel="noopener noreferrer">
-        Popov Vitaly
-      </a>
-    </footer>
-  </div >)
-}
-
-export default Home
+export default Home;
